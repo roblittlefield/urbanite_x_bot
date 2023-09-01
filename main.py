29ea97ab7d851a685079e6331139c093e8dee700
roblittlefield <github.0502@roblittlefield.com>
2023-09-01 11:00:13 -0700

    requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from google.cloud import secretmanager
-import functions_framework
+# from google.cloud import secretmanager
+# import functions_framework
+from google.cloud import storage
 import re
 import redis
 from requests_oauthlib import OAuth2Session
@@ -7,18 +8,18 @@ import requests
 import json
 from datetime import datetime, timedelta
 
+# Google Cloud Storage Urbanite Twitter Bot Bucket
+storage_client = storage.Client()
+bucket_name = "urbanite-twitter-bot-data"
+bucket = storage_client.bucket(bucket_name)
 
-def get_calls():
-    print("Getting calls...")
-    sf_data_url = 'https://data.sfgov.org/resource/gnap-fj3t.json'
-    sf_data_parameters = {
-        "$limit": 8000,
-    }
-    response = requests.get(url=sf_data_url, params=sf_data_parameters)
-    response.raise_for_status()
-    data_sf = response.json()
-    print(f"Number of retrieved SFPD calls (unfiltered): {len(data_sf)}\n")
-    return data_sf
+posted_tweets_file = "posted_tweets.csv"
+posted_tweets_blob = bucket.blob(posted_tweets_file)
+posted_tweets_existing_data = posted_tweets_blob.download_as_text() if posted_tweets_blob.exists() else ""
+
+tweets_wo_rt_file = "tweets_wo_rt.csv"
+tweets_wo_rt_blob = bucket.blog(tweets_wo_rt_file)
+tweet_wo_rt_existing_data = tweets_wo_rt_blob.download_as_text() if tweets_wo_rt_blob.exists() else ""
 
 
 def text_proper_case(text_raw):
@@ -34,31 +35,43 @@ def text_proper_case(text_raw):
 
 def find_tweet_id_by_cad_number(cad_number_try):
     try:
-        with open("posted_tweets.txt", 'r') as file:
-            lines = file.readlines()
+        if cad_number_try in posted_tweets_existing_data:
+            lines = posted_tweets_existing_data.split('\n')
             for line in lines:
                 parts = line.strip().split('-')
                 if len(parts) == 2 and parts[0].strip() == cad_number_try:
+                    print("Found original tweet, getting ID to reply to")
                     return parts[1].strip()
-            return None  # Cad number not found
-    except FileNotFoundError:
-        print(f"The file 'posted_tweets' was not found.")
-        return None
+            return False  # Cad number not found
+        else:
+            print(f"CAD number '{cad_number_try}' not found in the content.")
+            return False
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return False
+
+
+def get_calls():
+    sf_data_url = 'https://data.sfgov.org/resource/gnap-fj3t.json'
+    sf_data_parameters = {
+        "$limit": 8000,
+    }
+    response = requests.get(url=sf_data_url, params=sf_data_parameters)
+    response.raise_for_status()
+    data_sf = response.json()
+    print(f"Number of retrieved SFPD calls: {len(data_sf)}\n")
+    return data_sf
 
 
 def get_tweets(refreshed_token):
     calls = get_calls()
     call_tweets = []
-
     for call in calls:
-
-        if call["call_type_final"] == str(217) or call["call_type_final"] == str(219):  # 459 freq for testing, 217 = shooting
-
+        if call["call_type_final"] == str(217) or call["call_type_final"] == str(219) or call["call_type_final"] == str(212):  # 459 freq for testing, 217 = shooting
+            # Break for repeat tweets
             cad_number = call["cad_number"]
-            with open("posted_tweets.txt", "r") as file:
-                tweets = file.read()
-                if cad_number in tweets:
-                    continue
+            if cad_number in posted_tweets_existing_data:
+                continue
 
             received_date_string = call["received_datetime"]
             received_date = datetime.strptime(received_date_string, '%Y-%m-%dT%H:%M:%S.%f')
@@ -68,43 +81,58 @@ def get_tweets(refreshed_token):
             total_seconds = time_difference.total_seconds()
             minutes_ago = round(total_seconds / 60, 1)
             hours_ago = round(total_seconds / 3600, 1)
-            call_type_desc = call['call_type_final_desc'].title()
-
-            if call["onscene_datetime"]:
-                tweet_id = find_tweet_id_by_cad_number(cad_number)
-
+            if hours_ago > 24:
+                continue
+            try:
+                call_type_desc = call['call_type_final_desc'].title()
+            except KeyError:
+                call_type_desc = call['call_type_original_desc'].title()  # correct
+            print(f"{call_type_desc}: {hours_ago} hours ago. CAD {cad_number}")
+            try:
                 onscene_date_string = call["onscene_datetime"]
                 onscene_date = datetime.strptime(onscene_date_string, '%Y-%m-%dT%H:%M:%S.%f')
                 onscene_date_pst = onscene_date - timedelta(hours=7)
-
                 response_time_diff = onscene_date_pst - received_date_pst
                 response_time = round(response_time_diff.total_seconds() / 60)
 
-                print(f"Time ago: {hours_ago} hours. Response time: {response_time} mins. CAD {cad_number}")
-                if minutes_ago < 60:
-                    if tweet_id:
-                        new_reply = f"SFPD Priority {call['priority_final']}, Response time: {response_time}m"
-                        response = post_tweet_reply(tweet_id, new_reply, refreshed_token)
-                        if response.status_code == 201:
-                            tweet_id = json.loads(response.text)["data"]["id"]
-                            with open("posted_tweets.txt", "a") as file:
-                                file.write(f"{cad_number}-{tweet_id}\n")
-                            print(f"Posted Reply Tweet w RT CAD {cad_number} posted with ID: {tweet_id}")
-                        else:
-                            print("Tweet posting failed.")
+                tweet_wo_rt_id = find_tweet_id_by_cad_number(cad_number)
+                print(f"Response time: {response_time} mins")
+                if tweet_wo_rt_id:
+                    print(tweet_wo_rt_id)
+                    new_reply = f"Call answered, replying with response time: {response_time}m"
+                    response = post_tweet_reply(tweet_wo_rt_id, new_reply, refreshed_token)
+                    tweet_wo_rt_id = json.loads(response.text)["data"]["id"]
+                    if response.status_code == 201:
+                        mark_cad_posted(cad_number, tweet_wo_rt_id)
+                        print(f"Posted Reply Tweet w RT CAD {cad_number} posted with ID: {tweet_wo_rt_id}")
+                    elif response.status_code == 429:
+                        print("ERROR 429, MAXED OUT RATE LIMIT")
+                    elif response.status_code == 403:
+                        response_data = response.json()
+                        if 'errors' in response_data:
+                            for error in response_data['errors']:
+                                if 'code' in error and error['code'] == 187:
+                                    mark_cad_posted(cad_number, tweet_wo_rt_id)
+                                    print("Duplicate tweet detected, added to Posted Tweets. Error:", error['message'])
                     else:
-                        new_tweet = f"{call_type_desc} at {text_proper_case(call['intersection_name'])} in {call['analysis_neighborhood']} at {received_date_pst.strftime('%I:%M %p')}, Priority {call['priority_final']}, SFPD Response time: {response_time}m urbanitesf.netlify.app/?cad_number={call['cad_number'] }"
-                        call_tweets.append(new_tweet)
-            else:
-                if minutes_ago < 60:
-                    print(f"Time ago: {hours_ago} hours. No response time yet. CAD {cad_number}")
-                    new_tweet = f"{call_type_desc} at {text_proper_case(call['intersection_name'])} in {call['analysis_neighborhood']} at {received_date_pst.strftime('%I:%M %p')}, Priority {call['priority_final']}, SFPD currently responding urbanitesf.netlify.app/?cad_number={call['cad_number'] }"
+                        print(F"Tweet posting failed. RESPONSE STATUS CODE {response.status_code}")
+                else:
+                    new_tweet = f"{call_type_desc} at {text_proper_case(call['intersection_name'])} in {call['analysis_neighborhood']} at {received_date_pst.strftime('%I:%M %p')} ({minutes_ago}m ago), Priority {call['priority_final']}, SFPD Response time: {response_time}m urbanitesf.netlify.app/?cad_number={call['cad_number'] }"
                     call_tweets.append(new_tweet)
+            except KeyError:
+                print("No response time yet, adding tweet as wo rt")
+                new_tweet_wo = f"{call_type_desc} at {text_proper_case(call['intersection_name'])} in {call['analysis_neighborhood']} at {received_date_pst.strftime('%I:%M %p')} ({minutes_ago}m ago), Priority {call['priority_final']}, SFPD currently responding... urbanitesf.netlify.app/?cad_number={call['cad_number'] }"
+                call_tweets.append(new_tweet_wo)
 
-    print(f"Number of new shootings in last 60 minutes: {len(call_tweets)}\n")
-
+    print(f"Number of new stabbing/shootings/strong arm robberies: {len(call_tweets)}\n")
     return call_tweets
 
+
+def mark_cad_posted(cad_number, tweet_id):
+    posted_tweets_new_data = f"{cad_number}-{tweet_id},\n"
+    posted_tweets_updated_data = posted_tweets_existing_data + posted_tweets_new_data
+    posted_tweets_blob.upload_from_string(posted_tweets_updated_data)
+    print(f"Added call #{cad_number} with Tweet ID: {tweet_id}")
 
 def make_token():
     return OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scopes)
@@ -112,10 +140,6 @@ def make_token():
 
 def post_tweet(payload, token):
     print("Trying to Tweet!")
-    payload["geo"] = {
-        "coordinates": [-122.4194, 37.7749],
-        "type": "Point"
-    }
     return requests.request(
         "POST",
         "https://api.twitter.com/2/tweets",
@@ -128,13 +152,7 @@ def post_tweet(payload, token):
 
 
 def post_tweet_reply(tweet_id, tweet, token):
-    payload = {
-        "text": tweet,
-        "geo": {
-            "coordinates": [-122.4194, 37.7749],
-            "type": "Point"
-        }
-    }
+    payload = {"text": tweet}
     print("Trying to reply to an earlier Tweet!")
     return requests.request(
         "POST",
@@ -147,11 +165,15 @@ def post_tweet_reply(tweet_id, tweet, token):
     )
 
 
-client = secretmanager.SecretManagerServiceClient()
-client_id = client.access_secret_version(request={"name": "projects/urbanite-sf-twitter-bot/secrets/CLIENT_ID/versions/latest"}).payload.data.decode("UTF-8")
-client_secret = client.access_secret_version(request={"name": "projects/urbanite-sf-twitter-bot/secrets/CLIENT_SECRET/versions/latest"}).payload.data.decode("UTF-8")
-redirect_uri = client.access_secret_version(request={"name": "projects/urbanite-sf-twitter-bot/secrets/REDIRECT_URI/versions/latest"}).payload.data.decode("UTF-8")
-redis_url = client.access_secret_version(request={"name": "projects/urbanite-sf-twitter-bot/secrets/REDIS_URL/versions/latest"}).payload.data.decode("UTF-8")
+# client = secretmanager.SecretManagerServiceClient()
+# client_id = client.access_secret_version(request={"name": "projects/urbanite-sf-twitter-bot/secrets/CLIENT_ID/versions/latest"}).payload.data.decode("UTF-8")
+# client_secret = client.access_secret_version(request={"name": "projects/urbanite-sf-twitter-bot/secrets/CLIENT_SECRET/versions/latest"}).payload.data.decode("UTF-8")
+# redirect_uri = client.access_secret_version(request={"name": "projects/urbanite-sf-twitter-bot/secrets/REDIRECT_URI/versions/latest"}).payload.data.decode("UTF-8")
+# redis_url = client.access_secret_version(request={"name": "projects/urbanite-sf-twitter-bot/secrets/REDIS_URL/versions/latest"}).payload.data.decode("UTF-8")
+client_id = "NzFLTEozQkpBMHUwQnlMSU5YVWk6MTpjaQ"
+client_secret = "QbU4sMUkCLT92OnNqiG81cenEuxpEEHKYZ4CPf663qUjiDa0Dy"
+redirect_uri = 'http://127.0.0.1:5000/oauth/callback'
+redis_url = ("rediss://red-cjl8i95k5scs73dvjh70:7K8SENjbGrEUTG6XZ60uuyRjqSfLx6MI@oregon-redis.render.com:6379")
 
 r = redis.from_url(redis_url)
 token_url = "https://api.twitter.com/2/oauth2/token"
@@ -159,7 +181,7 @@ auth_url = "https://twitter.com/i/oauth2/authorize"
 scopes = ["tweet.read", "users.read", "tweet.write", "offline.access"]
 
 
-@functions_framework.cloud_event
+# @functions_framework.cloud_event
 def run_bot(cloud_event):
     twitter = make_token()
     t = r.get("token")
@@ -180,20 +202,24 @@ def run_bot(cloud_event):
     tweets = get_tweets(refreshed_token)
     for tweet in tweets:
         payload = {"text": tweet}
+        print(payload)
         response = post_tweet(payload, refreshed_token)
 
         if response.status_code == 201:
             tweet_id = json.loads(response.text)["data"]["id"]
             cad_number = payload["text"][-9:]
 
-            contains_response_time = "Response Time:" in tweet
+            contains_response_time = "SFPD Response time:" in tweet
             if not contains_response_time:
-                with open("tweets_without_rt.txt", "a") as file:
-                    file.write(f"{cad_number}-{tweet_id}\n")
-                print(f"Tweet w/o RT CAD {cad_number} posted with ID: {tweet_id}")
+                tweets_wo_rt_new_data = f"{cad_number}-{tweet_id},\n"
+                tweets_wo_rt_updated_data = tweet_wo_rt_existing_data + tweets_wo_rt_new_data
+                tweets_wo_rt_blob.upload_from_string(tweets_wo_rt_updated_data)
+                print(f"Tweet without RT, CAD {cad_number} posted with ID: {tweet_id}")
             else:
-                with open("posted_tweets.txt", "a") as file:
-                    file.write(f"{cad_number}-{tweet_id}\n")
-                print(f"Tweet w RT CAD {cad_number} posted with ID: {tweet_id}")
+                mark_cad_posted(cad_number, tweet_id)
+                print(f"Tweeted w RT, CAD {cad_number} posted with ID: {tweet_id}")
         else:
+            print(response.status_code)
             print("Tweet posting failed.")
+
+run_bot("Hello")
